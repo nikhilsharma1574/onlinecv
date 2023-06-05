9d5c0b4668d999126fd4e7d85f230f228f25abb6
Nikhil Sharma <nikhilsharma1574@gmail.com>
2023-06-05 13:30:58 +0530

    temp5

--- pages/test.js
+++ pages/test.js
@@ -1,19 +1,19 @@
-    // import Landing from '@/components/Landing'
-    // import Layout from '@/layouts/layout'
-    // import Head from 'next/head'
+    import Landing from '@/components/Landing'
+    import Layout from '@/layouts/layout'
+    import Head from 'next/head'
 
 
 
-    // export default function Home() {
-    // return (
-    // <>
-    //     <main id='element-bg' className='w-full h-screen bg-gradient-to-b from-[#0c274b] via-[#040615] to-[#040615]'>
-    //     <div id='element-bg'>
-    //     <Layout>
-    //         <Landing/>
-    //     </Layout>
-    //     </div>
-    //     </main>
-    // </>
-    // )
-    // }
+    export default function Home() {
+    return (
+    <>
+        <main id='element-bg' className='w-full h-screen bg-gradient-to-b from-[#0c274b] via-[#040615] to-[#040615]'>
+        <div id='element-bg'>
+        <Layout>
+            <Landing/>
+        </Layout>
+        </div>
+        </main>
+    </>
+    )
+    }
